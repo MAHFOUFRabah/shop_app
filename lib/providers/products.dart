46bf9eee960b5b57fd608019f2ff8a9f2f21fa15
MAHFOUFRabah <mahfouf.rabah@gmail.com>
2020-09-04 19:27:04 +0200

    Http request start

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:shop_app/providers/product.dart';
+import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -64,6 +67,14 @@ class Products with ChangeNotifier {
   */
 
   void addProduct(Product product) {
+    const url = 'https://flutter-udemy-shop-app-57d19.firebaseio.com/products.json';
+    http.post(url,body: json.encode({
+      'title': product.title,
+      'description' : product.description,
+      'imageUrl': product.imageUrl,
+      'price':product.price,
+      'isFavorite' :product.isFavorite,
+    }),);
     final newProduct = Product(
       title: product.title,
       description: product.description,
